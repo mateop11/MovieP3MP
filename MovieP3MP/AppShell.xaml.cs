23fa7acf10e9f3fcef10d75969f9c57a030b4002
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2025-01-21 23:18:31 -0500

    Agregar archivos de proyecto.

---
+++ MovieP3MP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieP3MP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MovieP3MP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MovieP3MP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
